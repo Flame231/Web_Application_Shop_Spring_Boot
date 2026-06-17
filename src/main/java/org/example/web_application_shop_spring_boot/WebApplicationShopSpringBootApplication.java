6736d170b65b0ea979e878af8c96a40aee84e1bd
System Administrator <flame5930@gmail.com>
2026-06-17 09:10:26 +0300

    Инициализация проекта

---
+++ src/main/java/org/example/web_application_shop_spring_boot/WebApplicationShopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.web_application_shop_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationShopSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationShopSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/web_application_shop_spring_boot/WebApplicationShopSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.web_application_shop_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationShopSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
